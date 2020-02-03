6af74df8e7f55fb62226a528e0a80c6835322f0a
Gino Messmer <git@ginomessmer.me>
2020-02-03 17:23:53 +0100

    Add project files.

---
+++ MagicHome/Light.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+
+namespace MagicHome
+{
+    public class Light
+    {
+        public bool Connected { get; set; }
+
+        public bool IsOn { get; set; }
+
+        public Color Color { get; set; }
+
+        public Light()
+        {
+        }
+    }
+}
